8c5ff901c1ad281d6ab50985df0406c9ec8639bd
KobiKano <97069265+KobiKano@users.noreply.github.com>
2025-09-13 16:34:13 -0500

    Added some testing skeleton code

---
+++ proj_cm4/src/tests/test_locks.c
@@ -0,0 +1,22 @@
+/**
+ * @file test_locks.c
+ * @author your name (you@domain.com)
+ * @brief Barebones testing of locks to ensure function calls execute correctly
+ * 
+ *              Proper testing has to be done in a running RTOS environement and will occur after the
+ *                  Unity tests complete
+ * 
+ * @version 0.1
+ * @date 2025-09-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "tests.h"
+#include "unity.h"
+
+void locks_test()
+{
+
+}
---
+++ proj_cm4/src/tests/test_sched.c
@@ -0,0 +1,18 @@
+/**
+ * @file test_sched.c
+ * @author your name (you@domain.com)
+ * @brief Test calls to the myrtos_schedule function to ensure proper tasks scheduled
+ * @version 0.1
+ * @date 2025-09-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "tests.h"
+#include "unity.h"
+
+void sched_test()
+{
+
+}
